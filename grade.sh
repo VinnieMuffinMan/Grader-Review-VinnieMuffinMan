26080344fd6b567df082ae02cb7c69f7ce98b3bd
VinnieMuffinMan <49897384+VinnieMuffinMan@users.noreply.github.com>
2023-02-16 17:22:05 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -31,7 +31,7 @@ java -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" org.junit.runn
 if [[ $? != 0 ]]
 then
     echo 'Failed tests'
-    cat testErr.txt
+    cat testErr.txt | grep -v -w "at"
     exit
 fi
 
